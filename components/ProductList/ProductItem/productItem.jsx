a4e0dd53007ee9ef97b37fff3603c967a41d3e9c
Bahodir8586 <bahodira213@gmail.com>
2021-09-17 17:05:05 +0500

    lazyloader

--- components/ProductList/ProductItem/productItem.jsx
+++ components/ProductList/ProductItem/productItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import NumberFormat from "react-number-format";
 import Stars from "../Stars";
 
@@ -6,11 +7,14 @@ const ProductItem = ({freeShipping, image, productName, rating, price}) => {
     return (
         <div className={"relative"}>
             {freeShipping &&
-            <span className={"font-openSans z-10 absolute top-2 left-2 bg-white text-4xs lg:text-2xs text-center px-2 py-1"}>
+            <span
+                className={"font-openSans z-10 absolute top-2 left-2 bg-white text-4xs lg:text-2xs text-center px-2 py-1"}>
                 Free shipping
             </span>}
             <div>
-                <img src={image} className={"w-full object-cover"} alt={productName}/>
+                <LazyLoad offset={800}>
+                    <img src={image} className={"w-full object-cover"} alt={productName}/>
+                </LazyLoad>
             </div>
             <h3 className={"font-oswald uppercase font-light text-2xs my-2"}>{productName}</h3>
             <Stars rating={rating}/>
--- components/SectionInformation/sectionInformation.jsx
+++ components/SectionInformation/sectionInformation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import VerticalLines from "./VerticalLines";
+import LazyLoad from 'react-lazyload';
 
 const SectionInformation = () => {
     return (
@@ -7,29 +8,37 @@ const SectionInformation = () => {
             <VerticalLines/>
             <div className={"lg:min-h-screen flex py-10 font-openSans text-2xs lg:text-2sm lg:flex-row flex-col"}>
                 <div className={"w-full lg:px-10"}>
-                    <h1 className={"text-3base lg:text-2xl uppercase font-semibold lg:font-normal mb-5 lg:mb-32 lg:mt-4"}>Rubber Hex Dumbbells</h1>
+                    <h1 className={"text-3base lg:text-2xl uppercase font-semibold lg:font-normal mb-5 lg:mb-32 lg:mt-4"}>Rubber
+                        Hex Dumbbells</h1>
                     <p className={"mb-10 lg:mb-20"}>
                         Again Faster® Rubber Hex Dumbbells offer one of the most effective ways to add variance to any
-                        training program. Functional dumbbell movements cultivate stability, coordination and strength in
-                        ways that cannot be achieved using a barbell. Lighter dumbbells are great tools for accommodating
-                        athletes with an injury or those not yet comfortable with a bar. Whether you are making your first
-                        equipment purchase or looking to expand your gym&apos;s capabilities, making dumbbells a priority will
+                        training program. Functional dumbbell movements cultivate stability, coordination and strength
+                        in
+                        ways that cannot be achieved using a barbell. Lighter dumbbells are great tools for
+                        accommodating
+                        athletes with an injury or those not yet comfortable with a bar. Whether you are making your
+                        first
+                        equipment purchase or looking to expand your gym&apos;s capabilities, making dumbbells a
+                        priority will
                         take your training to the next level.
                     </p>
                     <p>Need a way to organize your weights? Check out some of our Storage Options.</p>
                 </div>
                 <div className={"w-full"}>
-                    <img src={"/imgs/info-1.png"} className={"object-cover w-full h-full"}/>
+                    <LazyLoad><img src={"/imgs/info-1.png"}
+                                                className={"object-cover w-full h-full"}/></LazyLoad>
                 </div>
             </div>
 
             <div className={"lg:min-h-screen flex py-10 lg:py-20 lg:flex-row flex-col"}>
                 <div className={"w-full mb-6 lg:mb-0"}>
-                    <img src={"/imgs/info-2.png"} className={"object-cover w-full h-full"}/>
+                    <LazyLoad offset={100}><img src={"/imgs/info-2.png"}
+                                                className={"object-cover w-full h-full"}/></LazyLoad>
                 </div>
                 <div className={"text-2xs lg:text-2base w-full"}>
                     <div className={"lg:px-32"}>
-                        <h2 className={"font-openSans lg:mt-24 mb-4 lg:mb-6"}>Here’s what you need to know about our dumbbells</h2>
+                        <h2 className={"font-openSans lg:mt-24 mb-4 lg:mb-6"}>Here’s what you need to know about our
+                            dumbbells</h2>
                         <ul className={"lg:font-bold font-openSans"}>
                             <li className={"mb-4 flex"}>
                                 <span className={"text-gold mr-2"}>/</span>
@@ -51,10 +60,12 @@ const SectionInformation = () => {
 
             <div className={"lg:min-h-screen flex py-10 lg:py-20 lg:flex-row flex-col"}>
                 <div className={"w-full"}>
-                    <h2 className={"text-2xl lg:text-3xl font-bold lg:px-10 mb-6 lg:mt-24 lg:mr-32 uppercase"}>Perfect for indoor and outdoor workouts</h2>
+                    <h2 className={"text-2xl lg:text-3xl font-bold lg:px-10 mb-6 lg:mt-24 lg:mr-32 uppercase"}>Perfect
+                        for indoor and outdoor workouts</h2>
                 </div>
                 <div className={"w-full"}>
-                    <img src={"/imgs/info-3.png"} className={"object-cover w-full h-full"}/>
+                    <LazyLoad offset={800}><img src={"/imgs/info-3.png"}
+                                                className={"object-cover w-full h-full"}/></LazyLoad>
                 </div>
             </div>
         </section>
